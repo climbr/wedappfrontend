d51f33bac787a967c6e21eca7b16125478f47559
David Kucher <dkucher@davids-mbp.attlocal.net>
2017-05-09 13:44:33 -0500

    Numbers on main question

--- client/js/main.js
+++ client/js/main.js
@@ -1,15 +1,20 @@
+var i = 0;
+
 function main(){
+   $('#questionNumber').innerHTML= i+1;
     $('#yes').on('click', function(){
-        loadDoc();
-        createBoxYes();
+                 loadDoc(i);
+                 i++;
+        createBoxYes(i);
+        });
         $('.close').on('click', function(){
             deleteBox(this);
         });
-    });
     
     $('#no').on('click', function(){
-        loadDoc();
-        createBoxNo();
+         loadDoc(i);
+         i++;
+        createBoxNo(i);
         $('.close').on('click', function(){
             deleteBox(this);
         });
@@ -21,12 +26,12 @@ function main(){
     
 }
 
-function createBoxYes() {
-    var box = '<div class="panel lightblue"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="panel-heading">Question 1</div> <div class="panel-body text-center"> <p>blah blah blah</p> </div></div>';
-    $("#yesColumn").append(box);      // Insert new elements after img
+function createBoxYes(i) {
+    var box = '<div class="panel lightblue"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="panel-heading">Question </div> <div class="panel-body text-center"> <p>blah blah blah</p> </div></div>';
+    $("#yesColumn").append(box);// Insert new elements after img
 }
 
-function createBoxNo() {
+function createBoxNo(i) {
     var box = '<div class="panel coral"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="panel-heading">Question 1</div> <div class="panel-body text-center"> <p>blah blah blah</p> </div></div>';
     $("#noColumn").append(box);      // Insert new elements after img
 }
@@ -35,16 +40,17 @@ function createBoxNo() {
   $(x).parent().remove();
 }
 
-function loadDoc() {
+function loadDoc(i) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("question").innerHTML = this.responseText;
+       document.getElementById("question").innerHTML = this.responseText;
     }
   };
+   document.getElementById("questionNumber").innerHTML = i+2;
   xhttp.open("GET", "test.txt", true);
   xhttp.send();
-}
+    }
 
 
 $(document).ready(main);
