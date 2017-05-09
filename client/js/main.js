37b68d0acc593126709a9dfda0478a19c420d062
David Kucher <dkucher@davids-mbp.attlocal.net>
2017-05-08 19:07:34 -0500

    Delete Boxes

--- client/js/main.js
+++ client/js/main.js
@@ -2,32 +2,38 @@ function main(){
     $('#yes').on('click', function(){
         $('#question').text("answered");
         createBoxYes();
+        $('.close').on('click', function(){
+            deleteBox(this);
+        });
     });
     
     $('#no').on('click', function(){
         $('#question').text("answered");
         createBoxNo();
+        $('.close').on('click', function(){
+            deleteBox(this);
+        });
     });
     
-    //$('.close').on('click', function(){
-      //  deleteBox();
-    //});
+    $('.close').on('click', function(){
+       deleteBox(this);
+    });
 }
 
 function createBoxYes() {
-    var box = '<div class="panel lightblue"> <div class="panel-heading">Question 1</div> <div class="panel-body text-center"> <p>blah blah blah</p> </div></div>';
+    var box = '<div class="panel lightblue"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="panel-heading">Question 1</div> <div class="panel-body text-center"> <p>blah blah blah</p> </div></div>';
     $("#yesColumn").append(box);      // Insert new elements after img
 }
 
 function createBoxNo() {
-    var box = '<div class="panel coral"> <div class="panel-heading">Question 1</div> <div class="panel-body text-center"> <p>blah blah blah</p> </div></div>';
+    var box = '<div class="panel coral"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="panel-heading">Question 1</div> <div class="panel-body text-center"> <p>blah blah blah</p> </div></div>';
     $("#noColumn").append(box);      // Insert new elements after img
 }
 
-/* function deleteBox(box) {
-  $(box).remove();
+ function deleteBox(x) {
+  $(x).parent().remove();
 }
-*/
+
 
 $(document).ready(main);
 
