0f9b49e2e5a0b67d5dbd9945354dce245a760905
David Kucher <mrdkucher@gmail.com>
2017-05-10 20:31:20 +0000

    load json from file, fixed bug in toggle box

--- client/js/main.js
+++ client/js/main.js
@@ -5,9 +5,21 @@ var i = 0;
 // create a variable that is a javascript object, currently local 
 // but will be a link to an external doc eventually
 
-// var json = link(js/questions.json);
+var json = (function() {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "questions.json",
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })(); 
 
-var json = [
+var json1 = [
    {
         "title":"q1",
         "question":"question1"
@@ -29,36 +41,33 @@ var json = [
          "question":"question5"
     },
     {
-        "title":"",
+        "title": null,
         "question":"All Done!"
     }
 ];
-
+var responses =[];
 //create the main js function which will listen for clicks to update questions and place them in the right place
 
 function main(){
     updateQuestion(i);
     i++;
     $('#yes').on('click', function(){
-        //loadDoc(i);
-        createBoxYes(i-1);
-        updateQuestion(i);
-        i++;
-       $('.close').on('click', function(){
-            toggleBox(this);
-        });
+            createBoxYes(i-1);
+            responses[i-1]="yes";
+            updateQuestion(i);
+            i++;
     });
     $('#no').on('click', function(){
-         //loadDoc(i);
-        updateQuestion(i);
-        createBoxNo(i-1);
-        i++;
-        $('.close').on('click', function(){
-            toggleBox(this);
-        });
+            createBoxNo(i-1);
+            responses[i-1]="no";
+            updateQuestion(i);
+            i++;
     });
     
-    $('.close').on('click', function(){
+    $("#yesColumn").on("click", ".close", function(){
+       toggleBox(this);
+    });
+    $("#noColumn").on("click", ".close", function(){
        toggleBox(this);
     });
     
@@ -67,20 +76,21 @@ function main(){
 // if the yes box is clicked (in main) create a new box in the yes column with the original question
 
 function createBoxYes(i) {
-    var box = '<div class="panel lightblue"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="panel-heading">' + json[i].title + '</div> <div class="panel-body text-center"> <p>' + json[i].question + '</p> </div></div>';
+    var box = '<div class="panel lightblue"><button type="button" class="close" aria-label="Close">&times;</button><div class="panel-heading">' + json[i].title + '</div> <div class="panel-body text-center"> <p>' + json[i].question + '</p> </div></div>';
     $("#yesColumn").append(box);// Insert new elements after img
 }
 
 //if the no box is clicked (in main) create a new box in the no column with the original question
 
 function createBoxNo(i) {
-    var box = '<div class="panel coral"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="panel-heading">' + json[i].title + '</div> <div class="panel-body text-center"> <p>' + json[i].question + '</p> </div></div>';
+    var box = '<div class="panel coral"><button type="button" class="close" aria-label="Close">&times;</button><div class="panel-heading">' + json[i].title + '</div> <div class="panel-body text-center"> <p>' + json[i].question + '</p> </div></div>';
     $("#noColumn").append(box);      // Insert new elements after img
 }
 
 //show the i-th 
 
 function updateQuestion(i) {
+    //loadjson();
     var currentTitle = json[i].title;
     var currentQuestion = json[i].question;
     document.getElementById("title").innerHTML = currentTitle;
@@ -90,12 +100,17 @@ function updateQuestion(i) {
 //when the x is clicked, move the box to the alternate column
 
 function toggleBox(x){
-    if($(x).parent().parent().is($('#yesColumn'))){
+    if($(x).parent().is($('.lightblue'))){
         $(x).parent().addClass('coral');
         $(x).parent().removeClass('lightblue');
         $(x).parent().appendTo($("#noColumn"));
     }
-    else{
+    /*else if($(x).parent().is($('.coral'))){
+        $(x).parent().addClass('lightblue');
+        $(x).parent().removeClass('coral');
+        $(x).parent().appendTo($("#yesColumn"));
+    }*/
+    else {
         $(x).parent().addClass('lightblue');
         $(x).parent().removeClass('coral');
         $(x).parent().appendTo($("#yesColumn"));
@@ -103,17 +118,17 @@ function toggleBox(x){
 }
 
 /*
-function loadDoc(i) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+function loadjson() {
+  var xobj = new XMLHttpRequest();
+  xobj.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("question").innerHTML = this.responseText;
+//       callback(xhttp.responseText);
+         json1 = xobj.responseText;
     }
   };
-   document.getElementById("title").innerHTML = i+2;
-  xhttp.open("GET", "questions.json", true);
-  xhttp.send();
+  xobj.open("GET", "questions.json", true);
+  xobj.send(null);
     }
-*/
+*/    
 
 $(document).ready(main);
